d924318b253757956bf8845b931a9044ed948dad
Arina <arinapostnikova123@gmail.com>
2023-01-11 20:08:14 +0300

    Initial commit

---
+++ APub 2.0/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APub 2.0
+//
+//  Created by Arina Postnikova on 11.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
